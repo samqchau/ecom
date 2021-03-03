55791c27bd102e8e31e32b95c6d07bf548c3d709
samqchau <67344952+samqchau@users.noreply.github.com>
2021-03-02 16:42:21 -0800

    Cart Update
    Added cart functionality to hold items user wants to purchase. Displays cart information such as total price and quantity of each item. Cart items are stored in local storage so when exiting browser cart items are still remembered. Cart item quantity can be updated and removed.

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      quantity: qty,
+    },
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  const updatedCart = getState().cart.cartItems.filter(
+    (item) => item.product !== id
+  );
+  dispatch({ type: CART_REMOVE_ITEM, payload: updatedCart });
+  localStorage.setItem('cartItems', JSON.stringify(updatedCart));
+};
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,5 +1,13 @@
-import React, { useEffect } from 'react';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import GoBackButton from '../components/GoBackButton';
 
@@ -8,14 +16,22 @@ import { listProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ match, history }) => {
+  //redux
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
+  //component level state
+  const [quantity, setQuantity] = useState(1);
+
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
   }, [match.params.id, dispatch]);
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${quantity}`);
+  };
 
   return (
     <>
@@ -65,11 +81,33 @@ const ProductScreen = ({ match }) => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={quantity}
+                          onChange={(e) => {
+                            setQuantity(e.target.value);
+                          }}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
                     className='btn-block'
                     type='button'
                     disabled={product.countInStock <= 0}
+                    onClick={addToCartHandler}
                   >
                     Add to Cart
                   </Button>
